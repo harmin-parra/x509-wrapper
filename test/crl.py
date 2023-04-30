ae668e034790ad3c44ec8f5a80442568d5676150
Harmin Parra <harmin.parra@gmail.com>
2023-04-30 05:25:50 +0200

    Rename folder

--- test/crl.py
+++ test/crl.py
@@ -1,14 +1,10 @@
 from X509_wrapper import CRL
 
-print("\n##### crl #####\n")
-
-#CRL
-#crl = CRL.load_der_file("files/crl.der")
-#crl = CRL.load_pem_file("files/crl.pem")
+# CRL
+crl = CRL.load_der_file("resources/crl.der")
+crl = CRL.load_pem_file("resources/crl.pem")
 b64 = "MIIDSzCCAbMCAQEwDQYJKoZIhvcNAQELBQAwJjEPMA0GA1UECgwGSUQgUEtJMRMwEQYDVQQDDApJc3N1aW5nIENBFw0yMjA4MjAxMDA2MDJaFw0yMzA4MjAxMDA2MDJaMIIBJjA0AhUA36b291fmXjUFO307zn0mUXKJFa8XDTIyMDgwMTA5NTkyNVowDDAKBgNVHRUEAwoBBjAlAhRq+qpUJEfKonL6t8h9wwMStdjZoBcNMjIwODAyMTAwMTAxWjAzAhQcJa1u/0hRzzGeR9aK9sLbMAt6XRcNMjIwODAzMTAwMTI2WjAMMAoGA1UdFQQDCgEFME4CFQDgGSbAyUuS2PgZn1WAkdyfNJ5rJRcNMjIwODA0MTAwMTQzWjAmMAoGA1UdFQQDCgEEMBgGA1UdGAQRGA8yMDIwMDcwMzIzMDAwMFowQgIVAK6YZOoHk+k6PCa0j3/hLhZ79krDFw0yMjA4MDUxMDAxNTFaMBowGAYDVR0YBBEYDzIwMjAwNzA0MjMwMDAwWqAvMC0wCgYDVR0UBAMCAQUwHwYDVR0jBBgwFoAUiSHVAoKdwFixhxnibbS6Fnt0bXMwDQYJKoZIhvcNAQELBQADggGBAK8QAGZFNv1EFj/HICYNCSZXFwB13L7o5SAf0C/V3ueA5414FgErLDW3mL2pWgi+v3NyIv/vjcJGJprZbj6FZbM3D+K9JleocEtwETHGGmeGIo5XIZUNiMP6ZTtuccz6jwCW46LlFfd0qUCje8h6FcQCOAGKX9jdTwKjGvVLvPKbDKHyELl1QK1ctb1LCHQVQimBmJ2/AO6/6/iolDkTNCcXVoqT8sHCNp3BGTvbfXERwX6Rgg4AkOmcuGzAEc5+zTdvfvyWAqoo5WO3YTkVsawme9gHlwk/2GCX6tDKtBjycXVp1FpYtqw0KYoVigD4OqZSu6DBfS9VvAYzcFjJ68lFI2QbXXMhE/p/Hbc1f2Qvh7+yzDwWm45LGTh7U/HmvOgNfXE6GpDSCK/Nayn8geYyhDU39OwnxbTRMT4FtqppYA3ZZx60MbfsAdp8PAosI5egy6J5eEGFIgM2bz3yN93sEzfpzPryVm2Ng+/AHGKE87KmhMNPE7dM7b6+mYDfoA=="
 #crl = CRL.load_base64(b64)
-
-crl = CRL.load_der_file("files/empty.crl")
 
 print("IssuerDN:", crl.get_issuer_dn())
 print("Signature algorithm:", crl.get_signature_algorithm())
@@ -24,11 +20,10 @@ print(crl.dump("PEM"))
 
 
 # Delta CRL
-#delta = CRL.load_der_file("files/delta.der")
-#delta = CRL.load_pem_file("files/delta.pem")
+delta = CRL.load_der_file("resources/delta.der")
+delta = CRL.load_pem_file("resources/delta.pem")
 b64 = "MIIDSzCCAbMCAQEwDQYJKoZIhvcNAQELBQAwJjEPMA0GA1UECgwGSUQgUEtJMRMwEQYDVQQDDApJc3N1aW5nIENBFw0yMjA4MjAxMDA2MDJaFw0yMzA4MjAxMDA2MDJaMIIBJjA0AhUA36b291fmXjUFO307zn0mUXKJFa8XDTIyMDgwMTA5NTkyNVowDDAKBgNVHRUEAwoBBjAlAhRq+qpUJEfKonL6t8h9wwMStdjZoBcNMjIwODAyMTAwMTAxWjAzAhQcJa1u/0hRzzGeR9aK9sLbMAt6XRcNMjIwODAzMTAwMTI2WjAMMAoGA1UdFQQDCgEFME4CFQDgGSbAyUuS2PgZn1WAkdyfNJ5rJRcNMjIwODA0MTAwMTQzWjAmMAoGA1UdFQQDCgEEMBgGA1UdGAQRGA8yMDIwMDcwMzIzMDAwMFowQgIVAK6YZOoHk+k6PCa0j3/hLhZ79krDFw0yMjA4MDUxMDAxNTFaMBowGAYDVR0YBBEYDzIwMjAwNzA0MjMwMDAwWqAvMC0wCgYDVR0UBAMCAQUwHwYDVR0jBBgwFoAUiSHVAoKdwFixhxnibbS6Fnt0bXMwDQYJKoZIhvcNAQELBQADggGBAK8QAGZFNv1EFj/HICYNCSZXFwB13L7o5SAf0C/V3ueA5414FgErLDW3mL2pWgi+v3NyIv/vjcJGJprZbj6FZbM3D+K9JleocEtwETHGGmeGIo5XIZUNiMP6ZTtuccz6jwCW46LlFfd0qUCje8h6FcQCOAGKX9jdTwKjGvVLvPKbDKHyELl1QK1ctb1LCHQVQimBmJ2/AO6/6/iolDkTNCcXVoqT8sHCNp3BGTvbfXERwX6Rgg4AkOmcuGzAEc5+zTdvfvyWAqoo5WO3YTkVsawme9gHlwk/2GCX6tDKtBjycXVp1FpYtqw0KYoVigD4OqZSu6DBfS9VvAYzcFjJ68lFI2QbXXMhE/p/Hbc1f2Qvh7+yzDwWm45LGTh7U/HmvOgNfXE6GpDSCK/Nayn8geYyhDU39OwnxbTRMT4FtqppYA3ZZx60MbfsAdp8PAosI5egy6J5eEGFIgM2bz3yN93sEzfpzPryVm2Ng+/AHGKE87KmhMNPE7dM7b6+mYDfoA=="
 #delta = CRL.load_base64(b64)
-delta = CRL.load_der_file("files/empty.delta.crl")
 
 print("IssuerDN:", delta.get_issuer_dn())
 print("Signature algorithm:", delta.get_signature_algorithm())
--- test/key_pub.py
+++ test/key_pub.py
@@ -1,8 +1,8 @@
 from X509_wrapper import KEY
 
 # RSA
-key = KEY.load_public_key_pem_file("files/rsa.pem.pub.key")
-key = KEY.load_public_key_der_file("files/rsa.der.pub.key")
+key = KEY.load_public_key_pem_file("resources/rsa.pem.pub.key")
+key = KEY.load_public_key_der_file("resources/rsa.der.pub.key")
 b64 = "MIIBojANBgkqhkiG9w0BAQEFAAOCAY8AMIIBigKCAYEAtdktj74lBI096UamLayC756IXPNkTkgCtG8Vxts7sdFKbWFJxHdfcoMCtEwDQ0Mul7/ENG242srOeOlAlqT9oNAcUIklNi6Sg9JnlFxgsEe/t56sEFM1D44qXciBQybt4jJStU2REnU/LMDuD+aTlxNctDhOKM1Cb3jpblgn0WRqO6vVFcZtZaQFnxRyP+Tdh/7xDmf8+YlP49syp2jF52aEJio5Oks05fSmJyYto7ktJiFxlsH5LUbEIIJjGv8okFa8IpDli1RQWLCIKmpnGHd9MITXK5LWUq0Spl9Gl8gkRtknFsv4OzRt1m5GAHwYuRdD4fnDcy8LyuBxAb2fWSSrzFNUJ9Y+LEjeyDQNKZ/Xx6woOtdZUuLlmRNmuBJGKIsagS9Fs2E+vvitHLe47YR3P3GRntqGf7898Aa0+99l4FRPvltGuahxu6yOMDzJrSpmHh8vXcHBF/vn6ALlh8IrHwKEa8KwVpAmp6zQtLnJqLwZ244LCSY83baWeyuDAgMBAAE="
 key = KEY.load_public_key_base64(b64)
 
@@ -17,8 +17,8 @@ print(key.dump("PEM"))
 
 
 # ECDSA
-key = KEY.load_public_key_pem_file("files/ecdsa.pem.pub.key")
-key = KEY.load_public_key_der_file("files/ecdsa.der.pub.key")
+key = KEY.load_public_key_pem_file("resources/ecdsa.pem.pub.key")
+key = KEY.load_public_key_der_file("resources/ecdsa.der.pub.key")
 b64 = "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEYyYmM/2tiCNLVkJu5tWCvaq4X3hAS6wmwe/a+4NKs/NXAzBa+WKiAYhBwj7VZehu5OCxXVgyPuEQK9uSQTOe4w=="
 key = KEY.load_public_key_base64(b64)
 
