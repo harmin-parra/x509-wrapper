072bbc0254ca5cf124c75d72ae4ed8d814220fe6
Harmin Parra <harmin.parra@gmail.com>
2023-04-30 05:17:29 +0200

    Code refactoring

--- src/X509_wrapper/__init__.py
+++ src/X509_wrapper/__init__.py
@@ -1,4 +1,4 @@
-from .base import BASE, get_general_names
+from .base import BASE, decode_asn1_bytes, get_general_names
 from . import certificate as Certificate
 from . import crl as CRL
 from . import csr as CSR
